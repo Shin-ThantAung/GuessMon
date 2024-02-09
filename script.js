863f580a10cac428741c856a960b68c6c0029ff5
Shin Thant Aung <150310252+Shin-ThantAung@users.noreply.github.com>
2024-02-09 20:11:17 +0800

    finish up game and js

--- script.js
+++ script.js
@@ -1,45 +1,109 @@
 //game js
 
+//Initilizing the point for user
+var userPoint = 0;
+var userPointOutput = document.getElementById("userPoints");
+
+userPointOutput.innerHTML = `Points: ${userPoint}`;
 startGame();
 
 
-async function startGame(){
+async function startGame(x){
     try{
+        //
+        x.style.display = "none";
 
+        //initializing the point for user
+        let points = 5;
+
+        //Creating a random number between 1 and 1025
         var min = 1;
         var max = 1025;
         var randomNum = Math.floor(Math.random() * (max - min+ 1)) + min;
         
+        //fetch data from api
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
         const data = await response.json();
-        console.log(data);
 
-        
-        
+        //The pokemon name the user need to guess
         const randomPokemon = data.name;
-
-        const output = document.getElementById('outputText');
-
         console.log(randomPokemon);
+
+        //Output of when the user gets the correct answer
+        const output = document.getElementById('outputText');
+        const pkmSprite = document.getElementById('pokemonSprite');
+
+        //Output for hints given to user
+        const dexID = document.getElementById('pokedexID');
+        const type = document.getElementById('type');
+        const ability = document.getElementById('ability');
+
+
+        const pkmID = data.id;
+        const pkmType1 = data.types["0"];
+        const pkmType2 = data.types["1"];
+        const pkmAbility1 = data.abilities["0"];
+        const pkmAbility2 = data.abilities["1"];
+        const pkmAbility3 = data.abilities["2"];
+
+        //Displaying the id of the pokemon
+        dexID.innerHTML = `Pokedex ID: ${pkmID}`;
+
+        //Displaying the types of the pokemon
+        if (pkmType2 == null)
+        {
+            type.innerHTML = `Type: ${pkmType1.type["name"]}`;
+        }
+        else{
+            type.innerHTML = `Types: ${pkmType1.type["name"]} + ${pkmType2.type["name"]}`;
+        }
+
+
+        //Displaying the abilities of the pokemon
+        if (pkmAbility2 == null)
+        {
+            ability.innerHTML = `Ability: ${pkmAbility1.ability["name"]}`;
+        }
+        else if (pkmAbility3 == null)
+        {
+            ability.innerHTML = `Abilties: ${pkmAbility1.ability["name"]} + ${pkmAbility2.ability["name"]}`
+        }
+        else
+        {
+            ability.innerHTML = `Abilities: ${pkmAbility1.ability["name"]} + ${pkmAbility2.ability["name"]} + ${pkmAbility3.ability["name"]}`
+        }
 
         let btnSubmit = document.getElementById('btnSubmit');
 
 
+        //run this part if user click the submit button
         btnSubmit.addEventListener('click', function(e){
             e.preventDefault();
 
+            //get the input from the user and change it to lower case for comparison purpose
             let answer = document.getElementById("answer").value.toLowerCase();
 
-
-
+            //check if user enter the correct answer
             if (answer == randomPokemon)
             {
                 output.innerHTML = "Correct Answer!!!";
+
+                //display the pokemon sprite when correct
+                pkmSprite.src = data.sprites["front_default"];
+                pkmSprite.style.display = "block";
+
+                //Add the gained points
+                userPoint += points;
+
+                //Display the points 
+                userPointOutput.innerHTML = userPointOutput.innerHTML = `Points: ${userPoint}`;
+
+                x.style.display = "block";
             }
             else{
-                output.innerHTML = `Incorrect Answer.`;
+                points--;
+                output.innerHTML = `Incorrect Answer. ${points} points left.`;
             }
-
         })
 
     }
